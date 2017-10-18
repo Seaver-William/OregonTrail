335d61bc47ffa95ea54245687ec4466c6c1dc1d8
Moose <Moose@LAPTOP-VFMEA7PS>
2017-10-17 22:10:56 -0700

    10/17/17 test file passed for all 6 cases

--- OragonTrail/test/byu/cit260/oregontrail/control/riverCrossingControlTest.java
+++ OragonTrail/test/byu/cit260/oregontrail/control/riverCrossingControlTest.java
@@ -28,19 +28,84 @@ public class riverCrossingControlTest {
     }
 
     /**
-     * Test of dropSupplies method, of class riverCrossingControl.
+     * Test 1 of dropSupplies method, of class riverCrossingControl.
      */
     @Test
     public void testDropSupplies() {
-        System.out.println("dropSupplies");
-        double cartWeight = 395;
-        double suppliesDropped = 146;
+        System.out.println("dropSupplies #1");
+        double cartWeight = 250;
+        double suppliesDropped = 175;
         riverCrossingControl instance = new riverCrossingControl();
-        double expResult = 249;
+        double expResult = -1;
         double result = instance.dropSupplies(cartWeight, suppliesDropped);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
-    }
     
-}
+     /**
+        *Test Case #2
+        **/
+    System.out.println("dropSupplies #2");
+        cartWeight = 395 ;
+        suppliesDropped = 146;
+       
+        expResult = 249;
+        result = instance.dropSupplies(cartWeight, suppliesDropped);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    
+     /**
+        *Test Case #3
+        **/
+    System.out.println("dropSupplies #3");
+        cartWeight = 400 ;
+        suppliesDropped = 98;
+       
+        expResult = 302;
+        result = instance.dropSupplies(cartWeight, suppliesDropped);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    
+    /**
+        *Test Case #4
+        **/
+    System.out.println("dropSupplies #4");
+        cartWeight = 206 ;
+        suppliesDropped = 110;
+       
+        expResult = -1;
+        result = instance.dropSupplies(cartWeight, suppliesDropped);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        
+        /**
+        *Test Case #5
+        **/
+    System.out.println("dropSupplies #5");
+        cartWeight = 304 ;
+        suppliesDropped = 35;
+       
+        expResult = 269;
+        result = instance.dropSupplies(cartWeight, suppliesDropped);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        
+        /**
+        *Test Case #6
+        **/
+    System.out.println("dropSupplies #6");
+        cartWeight = 275;
+        suppliesDropped = 182;
+       
+        expResult = -1;
+        result = instance.dropSupplies(cartWeight, suppliesDropped);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    }
+
