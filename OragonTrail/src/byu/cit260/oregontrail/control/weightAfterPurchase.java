b55c8d7d0c51bab2e2f3d6328d2e09b0b44a562e
Ben <Ben@He-Man>
2017-10-21 19:49:45 -0600

    10/7/17 Hazards and Inventory

---
+++ OragonTrail/src/byu/cit260/oregontrail/control/weightAfterPurchase.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byu.cit260.oregontrail.control;
+
+/**
+ *
+ * @author Ben
+ */
+public class weightAfterPurchase {
+    
+    public double buySupplies (double currentSupplyWeight, double weightBought){
+        
+        if (currentSupplyWeight + weightBought > 800){
+            
+            return -1;}
+        
+        
+        double newSupplyWeight = (currentSupplyWeight + weightBought);
+            
+        return newSupplyWeight;
+       
+        
+        
+    } 
+}
---
+++ OragonTrail/test/byu/cit260/oregontrail/control/weightAfterPurchaseTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byu.cit260.oregontrail.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ben
+ */
+public class weightAfterPurchaseTest {
+    
+    public weightAfterPurchaseTest() {
+    }
+
+    /**
+     * Test of buySupplies method, of class weightAfterPurchase.
+     */
+    @Test
+    public void testBuySupplies() {
+        System.out.println("buySupplies");
+        double currentSupplyWeight = 500.0;
+        double weightBought = 200.0;
+        weightAfterPurchase instance = new weightAfterPurchase();
+        double expResult = 700.0;
+        double result = instance.buySupplies(currentSupplyWeight, weightBought);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
